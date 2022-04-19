d732f757dd0812986639d1bf2950074446340fcf
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2022-04-19 22:24:29 +0300

    add demo05-part2

---
+++ Day05/Demo/app.js
@@ -0,0 +1,112 @@
+// comments
+// this one line comment
+/*
+this comment is more than one line 
+test 
+*/
+
+// ctrl + ? this is a shourtcut for creating a comment
+
+// console.log("Hello from javascript");
+
+// Data type :
+
+//  1- value Types (primitives):
+// a. numbers
+// b. string
+// c. boolean
+
+// 2- Reference Types (Objects) :
+// a. Objects
+// b. Functions
+// c. Arrays
+
+// String => "Israa"  , 'Israa'  , "25"
+// Number => 25
+// Boolean => false , true
+
+// variables
+// var
+// let
+// const
+var name = "Israa";
+var age = 25;
+
+// console.log(name);
+// console.log(age);
+
+// alert("Hello my name is " + name);
+
+var userName = prompt("who are you ?");
+// var userAge = prompt("how old are you ?");
+
+// console.log(userName);
+
+// confirm H.W
+
+// comparison operations
+// >
+// <
+// <=
+// >=
+// !=
+// ==  compare the value
+// === identical => compare the dataType and the value
+
+// Logical operations
+// 1- And &&  => one case for true result which is (both conditions are true )
+
+// 2- OR ||  => one case for false result which is (both conditions are false )
+
+// 3- Not !
+
+// console.log(25 == "25" ); true
+// console.log(25 === "25" ); false
+
+// console.log(name == "Israa" || name == "Ahmad" && age == 25 )
+
+// conditional statments (if + switch)
+// 1- if statment
+// if(condition){
+//   code the true case
+// }else{
+//   code the false case
+// }
+
+// if(condition 1){
+//   code the true case for condition 1
+// }else if(condition 2){
+//   code the true case for condition 2
+// }else{
+//   code the false case
+// }
+
+// var password = prompt("Enter your passwrd")
+// if(password == 1234){
+//   alert("Welcome to my website")
+// }else{
+//   alert("wrong password, try again ")
+// }
+
+// if(userAge >=25){
+//   alert("Hello");
+// }else if(userAge >= 10 && userAge < 25 ){
+//   alert("welcome");
+// }else{
+//   alert("go home");
+// }
+
+// 2- switch
+
+// switch(userName){
+//   case "Israa":
+//     alert("hello")
+//     break;
+//   case "Ahmad" :
+//     alert ("welcome");
+//     break;
+//   default:
+//     alert ("test");
+// }
+
+// H.W : when do we use if and when do we use switch
